ba8a8f544924dd9b52c33d87baa61af0fe5bf601
gsaldanha2 <gsaldanha2@gmail.com>
2014-01-09 09:03:40 -0800

    Main game file

---
+++ Game.java
@@ -0,0 +1,6 @@
+public class Game {
+  //create the game main file
+  //add the main method to create the frame
+  //use this as the superclass but do not extend
+  //do not forget to extend Canvas or JFrame. Up to you
+}
